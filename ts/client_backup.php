19195538ab4417f1582fc9e57d74a83593b02823
Austin Rocha <arocha@Austin-Rochas-MacBook-Pro.local>
2016-02-01 08:06:35 -0500

    initial commit

---
+++ ts/client_backup.php
@@ -0,0 +1,24 @@
+<html>
+<head>
+<link rel="shortcut icon" href="../teamspeak/teamspeak.ico">
+<title>TSStatus</title>
+<link rel="stylesheet" type="text/css" href="../teamspeak/tsstatus.css" />
+</head>
+<body>
+<p><br><br><br><br></p>
+<?php
+require_once("../teamspeak/tsstatus.php");
+$tsstatus = new TSStatus("www.au1st3in.net", 10011);
+$tsstatus->useServerPort(9987);
+$tsstatus->imagePath = "../teamspeak/img/";
+$tsstatus->timeout = 2;
+$tsstatus->setLoginPassword("LOGIN", "PASSWORD");
+$tsstatus->setCache(2);
+$tsstatus->hideEmptyChannels = true;
+$tsstatus->hideParentChannels = true;
+$tsstatus->showNicknameBox = false;
+$tsstatus->showPasswordBox = false;
+echo $tsstatus->render();
+?>
+</body>
+</html>
