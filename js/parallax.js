aee1e941761d352a19e6d81dc4e6800431d17c33
Austin Rocha <Au1st3in@users.noreply.github.com>
2016-02-02 13:41:43 -0500

    min

--- js/parallax.js
+++ js/parallax.js
@@ -1,8 +1,6 @@
 (function($){
   $(function(){
-
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
-
-  }); // end of document ready
-})(jQuery); // end of jQuery name space
+  });
+})(jQuery);
