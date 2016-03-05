5f8c112332475104bac5433180e98912e023469b
Austin Rocha <Au1st3in@users.noreply.github.com>
2016-03-05 15:55:05 -0500

    format

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
-<?php
-  $serverIP = $_SERVER["REMOTE_ADDR"];
-  $serverPort = "9987";
-  $serverDNS = "ts.au1st3in.net";
-  $serverDDNS = "www.au1st3in.net";
-?>
+  <?php
+    $serverIP = $_SERVER["REMOTE_ADDR"];
+    $serverPort = "9987";
+    $serverDNS = "ts.au1st3in.net";
+    $serverDDNS = "www.au1st3in.net";
+  ?>
   <head>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
